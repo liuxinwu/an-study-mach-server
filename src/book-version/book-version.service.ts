9f5f05bb9854462a15b1ac3991811cb4883d4188
xinwu.liu <xinwu.liu@corp.to8to.com>
2024-01-15 15:33:59 +0800

    feat: 排除不需要显示的字段

--- src/book-version/book-version.service.ts
+++ src/book-version/book-version.service.ts
@@ -15,8 +15,13 @@ export class BookVersionService {
   }
 
   async find(_id?: Schema.Types.ObjectId): Promise<BookVersionDocument[]> {
+    // 排除不显示字段 0 不显示  1 显示
+    const exculdField = { __v: 0 };
     if (!_id)
       return this.bookVersionModel.aggregate([
+        {
+          $project: exculdField,
+        },
         {
           $lookup: {
             from: 'bookclassifies', // 离谱集合名一定要按照数据库的表来, 注意 mongodb 对集合名的转换
@@ -25,7 +30,14 @@ export class BookVersionService {
             as: 'bookClassifyInfo',
           },
         },
+        {
+          $project: {
+            'bookClassifyInfo.__v': 0,
+            'bookClassifyInfo.sourceId': 0,
+            'bookClassifyInfo.pId': 0,
+          },
+        },
       ]); // 聚合查询中进行数据关联
-    return this.bookVersionModel.find({ _id });
+    return this.bookVersionModel.find({ _id }, exculdField);
   }
 }
